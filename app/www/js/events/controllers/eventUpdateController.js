e01c32c82f2b763bf274e92fb850f616f28d2c47
mdeswer1 <mike.de.swert@staff.telenet.be>
2016-03-18 14:43:56 +0100

    edited list view

--- app/www/js/events/controllers/eventUpdateController.js
+++ app/www/js/events/controllers/eventUpdateController.js
@@ -1,11 +1,21 @@
-angular.module('events').controller('eventUpdateController', ['$stateParams', '$location', 'eventsService',
-    function($stateParams, $location, eventsService) {
+angular.module('events').controller('eventUpdateController', ['$cordovaCalendar', '$stateParams', '$location', 'eventsService',
+    function($cordovaCalendar, $stateParams, $location, eventsService) {
         var ctrl = this;
         ctrl.oldEvent = eventsService.getEvent($stateParams.eventId);
         ctrl.event = ctrl.oldEvent.copy();
 
         ctrl.updateEvent = function() {
             eventsService.updateEvent(ctrl.oldEvent, ctrl.event);
+            $cordovaCalendar.modifyEvent({
+                title: ctrl.oldEvent.title,
+                notes: ctrl.oldEvent.description,
+                startDate: ctrl.oldEvent.getStartDate(),
+                endDate: ctrl.oldEvent.getEndDate(),
+                newTitle: ctrl.event.title,
+                newNotes: ctrl.event.description,
+                newStartDate: ctrl.event.getStartDate(),
+                newEndDate: ctrl.getEndDate()
+            });
             $location.path('/tab/events/' + ctrl.event.id);
         }
     }
