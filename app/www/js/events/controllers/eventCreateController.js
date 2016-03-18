292f41c38130821cf55ed6c1d6599e2fe5abebba
Briers Tom <tom.briers@staff.telenet.be>
2016-03-18 12:01:02 +0100

    photo

--- app/www/js/events/controllers/eventCreateController.js
+++ app/www/js/events/controllers/eventCreateController.js
@@ -1,5 +1,5 @@
-angular.module('events').controller('eventCreateController', ['$location', 'eventsService', 'events',
-    function($location, eventsService, events) {
+angular.module('events').controller('eventCreateController', ['$location', '$cordovaCamera', '$cordovaCapture', 'eventsService', 'events',
+    function ($location, $cordovaCamera, $cordovaCapture, eventsService, events) {
         var ctrl = this;
         init();
 
@@ -7,9 +7,43 @@ angular.module('events').controller('eventCreateController', ['$location', 'even
             ctrl.event = events.create();
         }
 
-        ctrl.saveEvent = function() {
+        ctrl.saveEvent = function () {
             eventsService.addEvent(ctrl.event);
             $location.path('/tab/events');
-        }
+        };
+
+        ctrl.takePhoto = function () {
+            var options = {
+                quality: 75,
+                destinationType: Camera.DestinationType.DATA_URL,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                allowEdit: true,
+                encodingType: Camera.EncodingType.JPEG,
+                targetWidth: 300,
+                targetHeight: 300,
+                popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: false
+            };
+
+            $cordovaCamera.getPicture(options).then(function (imageData) {
+                console.log(imageData);
+                //$scope.imgURI = "data:image/jpeg;base64," + imageData;
+            }, function (err) {
+                // An error occured. Show a message to the user
+            });
+        };
+
+        document.addEventListener("deviceready", function () {
+            console.log('aha');
+            ctrl.takePicture = function () {
+                var options = {limit: 3};
+
+                $cordovaCapture.captureImage(options).then(function (imageData) {
+                    console.log(imageData);
+                }, function (err) {
+                    console.log(err);
+                });
+            };
+        }, false);
     }
 ]);
